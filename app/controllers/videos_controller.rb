57eee8eaab488cbc7c661abd7dc74495e4a57cd0
goutham vel <gouthamvel@gmail.com>
2014-12-17 01:15:53 +0530

    add secure url for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -16,7 +16,7 @@ class VideosController < ApplicationController
   end
 
   def new
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201)
     @video = Video.new
     respond_with(@video)
   end
--- app/models/video.rb
+++ app/models/video.rb
@@ -6,6 +6,11 @@ class Video < ActiveRecord::Base
   validates_presence_of :title, message: "can't be blank"
   validates_associated :user
 
+  def authenticated_url
+    object = S3_BUCKET.objects[video_url.gsub "//dax-video-app-local.s3.amazonaws.com/", ""]
+    object.url_for(:get, { :expires => 20.minutes.from_now, :secure => true }).to_s
+  end
+
   def markers
     hash = comments.as_json(Comment::JSON_MARKER_INCLUDE)
     hash.map do |c|
